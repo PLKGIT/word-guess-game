9d2e23d0f294baf990bbe8bea39279cb8376f32c
Lammie <plkelly@yahoo.com>
2019-12-15 22:56:21 -0800

    Created and tested welcomeMsg() and initSession().

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,100 @@
 // Create Global Variables
-var userName="";
+var userName=null;
 var countWins = 0;
-var progress=12;
-var countWrongGuesses = 0;
+var countProgress=12;
+var countWrongGuesses=0;
 var arrUsedLetters = [];
 var arrGameWord =[];
+var arrProgress =[];
 var arrRandomWords = ["adjust","look","fare","bush","disposition","fade","monopoly","carve","fashionable","expand","trunk","sail","enter","soak","inspector","woman","transform","loop","shortage","sink","sofa","poor","ignorance","pride","marriage","relate","weed","accident","diplomat","cottage","attention","praise","danger","literature","consumption","umbrella","west","complain","overcharge","dash","trace","tissue","constraint","sunday","quarter","visit","organization","atmosphere","contain","index","finger","concession","joint","light","partner","abandon","pasture","hypnotize","dialogue","license","office"];
 var arrRandomQuotes = ["Just one more thing.","I knew you could do it.", "I can't think in this coat.", "I'm hanging up.  You can keep talkin' but I'm gonna hang up.", "I can't swim, I don't even like a deep tub.", "I respect your talent, but I don't like anything else about you.","Before coffee, I'm up and walkin' around but I'm not awake.s"];
 var arrRandomImages = [];
 
 // Create variables to set document contents during the game
+/*Tested*/
 var detective = document.getElementById("playerName");
 var gameBoard = document.getElementById("wordPanel");
 var usedBoard = document.getElementById("usedPanel");
 
-// Set a flag for anykey listener
-var anyKeyFlag = true;
+var letterBtn;
+
+// Variables for Game Music
+/*Tested*/
+
+var audioWin = new Audio("assets/audio/columbo_theme.mp3");
+var audioLose = new Audio("assets/audio/womp_womp.mp3");
+
+
+
+    // Set a flag for anykey listener so that I can turn it off after the initial click
+    var anyKeyFlag = true;
 
     //Prompt player for their name.
     /*Tested*/
     userName = prompt("May I know your name?");
 
-    //Call welcomeMsg() function
-    /*Tested*/
-    initSession ();
-
     //Put player's name in the welcome message
     /*Tested*/
+    welcomeMsg();
+
+    function welcomeMsg(){
+        //Check whether User input is null or contains a number. 
+         var ltrs = /\d/;
+
+         if (userName === null || ltrs.test(userName)==true){
+ 
+            //If input is null or a number, do not display userName
+ 
+             detective.textContent = "!";
+         } 
+         else {
+ 
+             //If input is NOT null or a number, display userName (in uppercase) in the welcome message
+ 
+             detective.textContent = " " + userName.toUpperCase() + "!";
+         }
+
+        //Call initSession()
+        initSession();
+    }
+
+
+    //Initialize the Session
+    /* Tested */
 
     function initSession () {
-        
+
         //Hide Columbo Image/Quote Card
         document.getElementById("winnerPanel").style.display = "none";
-        //Display "Click Any Key" message
-        document.getElementById("pressIt").style.display = "block";
-        //Clear Game Word Board
-        gameBoard.textContent = null;
-        //Clear Used Letters Board
-        usedBoard.textContent = null;
 
-        //Check whether User input is null or contains a number. 
-        
-        var ltrs = /\d/;
+        //Select new random word from arrRandomWords
+        arrGameWord = arrRandomWords [Math.floor(Math.random() * arrRandomWords.length - 1) + 1];
 
-        if (userName === null || ltrs.test(userName)==true){
+        console.log(arrGameWord);
 
-            //If input is null or a number, do not display userName
+        var theDiv;
+        var content;
 
-            detective.textContent = "!";
-        } 
-        else {
-
-            //If input is NOT null or a number, display userName (in uppercase) in the welcome message
-
-            detective.textContent = " " + userName.toUpperCase() + "!";
-        }
+        for (var i = 0; i < arrGameWord.length; i++) {
+            //create blanks for letters
+            theDiv = document.getElementById("wordPanel");
+            content = "<button class='btn btn-outline-info btn-lg border-top-0 border-left-0 border-right-0 id='"
+            content += i + "'> </button> "
+            theDiv.innerHTML += content; 
+          }
+       
     }
 
     // Initial event listener for users to click any key
     /*Tested*/
     document.onkeyup = function() {
         if (anyKeyFlag == true) {
-             //Hide "Click Any Key" message
+            //Hide "Click Any Key" message
              document.getElementById("pressIt").style.display = "none";
-             //Turn off anykey flag
-                 anyKeyFlag=false;
-
+            //Turn off anykey flag
+            anyKeyFlag=false;
+            //Call pickletter()
+            pickLetter();
         } else {
             //Should stop the listener from doing anything after the initial click
             return;
@@ -98,25 +127,42 @@ function youWon() {
     //Select Random image from arrRandomImage
     //Select Random quote from arrRandomQuote
     //Display Columbo Card
-    //Play Columbo Theme
-    //Add one to playerWins
-    //Alert "You cracked the case!"
-    //Call newGame()
     document.getElementById("winnerPanel").style.display = "block";
+    //Play Columbo Theme
+    audioWin.play();
+    //Add one to playerWins
+    countWins++;
+    //Alert "You cracked the case!"
+    alert("You cracked the case Detective!") ; 
+    //Call newGame()
+    newGame();
+
 }
 
 function youLost() {
-    //Play whomp whomp
-    //Alert "Sorry, better luck next time."
+
+    //Hide Columbo Image/Quote Card
     document.getElementById("winnerPanel").style.display = "none";
+    //Play whomp whomp
+    audioLose.play();
+    //Alert "Sorry, better luck next time."
+    alert("Sorry, better luck next time Detective!") ; 
     //Call newGame()
+    newGame();
 }
 
 function newGame() {
+    //Clear Game Word Board
+        gameBoard.textContent = null;
+    //Clear Used Letters Board
+        usedBoard.textContent = null;
     //Reset Wrong Guesses counter and Used Letter Array
         countWrongGuesses = 0;
         arrUsedLetters = [];
-    //Reset Status Bar
+    //Reset Status Bar to 12 chances
+
+    //Reset Progress Counter to 12
+        countProgress=12;
     //Select new random word from arrRandomWords
     //Create dashes for each letter of the Game Word
     //call pickLetter()
