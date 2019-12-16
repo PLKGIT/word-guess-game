c52625e3dcbf88867d9314d298f69b5ebecb89c1
Lammie <plkelly@yahoo.com>
2019-12-16 09:07:53 -0800

    Fixed a typo impacting an images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Create Global Variables
-var userName = null;
+var userName;
 var countWins = 0;
 var countProgress = 12;
 var countWrongGuesses = 0;
@@ -9,7 +9,7 @@ var arrCorrectLetters = [];
 var arrGameWord = [];
 var arrProgress = [[12, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-success' style='width: 100%' aria-valuenow='12' >12</div>"], [11, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-success' style='width: 91%' aria-valuenow='11' >11</div>"], [10, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-success' style='width: 83%' aria-valuenow='01' >10</div>"], [9, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-info' style='width: 75%' aria-valuenow='9' >9</div>"], [8, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-info' style='width: 66%' aria-valuenow='8' >8</div>"], [7, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-info' style='width: 58%' aria-valuenow='7' >7</div>"], [6, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-warning' style='width: 50%' aria-valuenow='6' >6</div>"], [5, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-warning' style='width: 41%' aria-valuenow='5' >5</div>"], [4, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-warning' style='width: 33%' aria-valuenow='4' >4</div>"], [3, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-danger' style='width: 25%' aria-valuenow='3' >3</div>"], [2, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-danger' style='width: 16%' aria-valuenow='2' >2</div>"], [1, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-danger' style='width: 1%' aria-valuenow='1' >1</div>"], [0, "<div role='progressbar' aria-valuemin='0' aria-valuemax='12' class='progress-bar bg-danger' style='width: 1%' aria-valuenow='0' >0</div>"]];
 var arrRandomWords = ["adjust", "look", "fare", "bush", "disposition", "fade", "monopoly", "carve", "fashionable", "expand", "trunk", "sail", "enter", "soak", "inspector", "woman", "transform", "loop", "shortage", "sink", "sofa", "poor", "ignorance", "pride", "marriage", "relate", "weed", "accident", "diplomat", "cottage", "attention", "praise", "danger", "literature", "consumption", "umbrella", "west", "complain", "overcharge", "dash", "trace", "tissue", "constraint", "sunday", "quarter", "visit", "organization", "atmosphere", "contain", "index", "finger", "concession", "joint", "light", "partner", "abandon", "pasture", "hypnotize", "dialogue", "license", "office"];
-var arrRandomQuotes = ["Just one more thing.", "I knew you could do it.", "I can't think in this coat.", "I'm hanging up.  You can keep talkin' but I'm gonna hang up.", "I can't swim, I don't even like a deep tub.", "I respect your talent, but I don't like anything else about you.", "Before coffee, I'm up and walkin' around but I'm not awake.s"];
+var arrRandomQuotes = ["Just one more thing.", "I knew you could do it.", "I can't think in this coat.", "I'm hanging up.  You can keep talkin' but I'm gonna hang up.", "I can't swim, I don't even like a deep tub.", "I respect your talent, but I don't like anything else about you.", "Before coffee, I'm up and walkin' around but I'm not awake."];
 var arrRandomImages = ["assets/images/01_columbo.png", "assets/images/02_columbo.jpg", "assets/images/03_columbo.jpg", "assets/images/04_columbo.jpg", "assets/images/05_columbo.jpg", "assets/images/06_columbo.jpg", "assets/images/07_columbo.jpg", "assets/images/08_columbo.jpg", "assets/images/09_columbo.jpg", "assets/images/10_columbo.jpg", "assets/images/11_columbo.jpg", "assets/images/12_columbo.jpg", "assets/images/13_columbo.jpg", "assets/images/14_columbo.jpg", "assets/images/15_columbo.jpg", "assets/images/16_columbo.jpg", "assets/images/17_columbo.jpg", "assets/images/18_columbo.jpg", "assets/images/19_columbo.jpg", "assets/images/20_columbo.jpg", "assets/images/21_columbo.jpg", "assets/images/22_columbo.jpg", "assets/images/23_columbo.jpg", "assets/images/24_columbo.jpg", "assets/images/25_columbo.jpg", "assets/images/26_columbo.jpg", "assets/images/27_columbo.jpg", "assets/images/28_columbo.jpg"];
 
 // Create variables to set document contents during the game
@@ -19,7 +19,7 @@ var winTotal = document.getElementById("playerWins");
 var gameBoard = document.getElementById("wordPanel");
 var usedBoard = document.getElementById("usedPanel");
 var remLetters = document.getElementById("remainPanel");
-// var revLetters = document.getElementById("wordPanel");
+var revLetters = document.getElementById("revealed");
 var winQuote = document.getElementById("quotes");
 
 
@@ -42,7 +42,7 @@ function welcomeMsg() {
     //Check whether User input is null or contains a number. 
     var ltrs = /\d/;
 
-    if (userName === null || ltrs.test(userName) == true) {
+    if (userName === null || ltrs.test(userName) === true) {
 
         //If input is null or a number, do not display userName
 
@@ -122,8 +122,11 @@ function pickLetter() {
         //Show Word Panel;
         document.getElementById("wordPanel").style.display = "block";
 
-        //Show Progress Bar
+        //Show Remaining Tries
         document.getElementById("remainPanel").style.display = "block";
+
+        //Show Winner Panel
+        document.getElementById("winnerPanel").style.display = "block";
 
         //capture player input
 
@@ -134,7 +137,7 @@ function pickLetter() {
             youLost();
 
         }
-        else if ((countProgress > 0) && (arrCorrectLetters.length === arrGameWord.length)) {
+        else if (countProgress > 0 && arrCorrectLetters.length === arrGameWord.length) {
 
             youWon();
         }
@@ -188,20 +191,20 @@ function youWon() {
     //Select Random quote from arrRandomQuote
     var genQuote = arrRandomQuotes[Math.floor(Math.random() * arrRandomQuotes.length - 1) + 1];
     winQuote.textContent = genQuote;
-    // console.log(genQuote);
-    // console.log(winQuote);
+    console.log(genQuote);
+    console.log(winQuote);
 
     // Select Random image from arrRandomImage
     // <img class="card-img-top" src="assets/images/11_columbo.jpg" alt="Picture of Columbo"></img>
     var genImage = arrRandomImages[Math.floor(Math.random() * arrRandomImages.length - 1) + 1];
-    console.log(genImage);
+    console.log(elem);
 
     var elem = document.createElement("img");
     elem.setAttribute("src", genImage);
     elem.setAttribute("class", "card-img-top");
     elem.setAttribute("alt", "Picture of Columbo");
-    document.getElementById("images").With(elem);
-    // console.log(elem);
+    document.getElementById("images").replaceWith(elem);
+    console.log(elem);
 
     //Display Columbo Card
     document.getElementById("winnerPanel").style.display = "block";
